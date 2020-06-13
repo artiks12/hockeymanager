6fc74376f1e40e3dce787b8bbec467998b3ef7b3
artiks12 <58003415+artiks12@users.noreply.github.com>
2020-06-13 21:42:28 +0300

    Update players.blade
    End of day

--- resources/views/players.blade.php
+++ resources/views/players.blade.php
@@ -16,7 +16,10 @@
             <div class="card-header">Players</div>
 
             <div class="card-body">
-                
+                @foreach($players as $player)
+                <?php if($player->team!=NULL){$team=App\Team::where('id','=',$player->team)->first();} ?>
+                <a href='{{url('players='.$player->id)}}'>{{$player->name}} {{$player->surname}} @if($player->position=='G') Goalie @elseif($player->position=='D') Defenceman @else Forward @endif @if($player->team!=NULL){{$team->teamName}} @endif </a><br>
+                @endforeach
             </div>
         </div>
     </div>
