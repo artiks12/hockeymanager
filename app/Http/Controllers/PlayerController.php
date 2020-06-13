c3227290158054516e1a63a8a8e036c8d3d18019
artiks12 <58003415+artiks12@users.noreply.github.com>
2020-06-13 21:34:17 +0300

    PlayerController update
    End of day

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Player;
+use App\Team;
+use App\Field;
+use App\Goalie;
 
 class PlayerController extends Controller
 {
@@ -13,7 +17,8 @@ class PlayerController extends Controller
      */
     public function index()
     {
-        return view('players');
+        $players = Player::all()->sortBy('surname')->sortBy('name');
+        return view('players', array('players' => $players));
     }
 
     /**
@@ -25,7 +30,41 @@ class PlayerController extends Controller
     {
         //
     }
-
+    
+    public function sign($id,$player)
+    {
+        $player = Player::where('id','=',$player)->first();
+        return view ('contracts',array('player' => $player));
+    }
+    
+    public function get(Request $request, $id, $team)
+    {
+        $player = Player::where('id','=',$id)->first();
+        $rules = array(
+            'number' => 'required|integer|gt:0|lt:100',
+            'money' => 'required|numeric|gte:0',
+            'years' => 'required|integer|gt:0',
+            );   
+        $this->validate($request, $rules);
+        $player->team = $team;
+        $player->number = $request->number;
+        $player->Years = $request->years;
+        $player->Cap = ($request->money)/($request->years);
+        $player->save();
+        return redirect('/team='.$team.'/page=contracts');
+    }
+    
+    public function release($id)
+    {
+        $player = Player::where('id','=',$id)->first();
+        $team = $player->team;
+        $player->team=NULL;
+        $player->number=NULL;
+        $player->Cap=NULL;
+        $player->Years=NULL;
+        $player->save();
+        return redirect('/team='.$team.'/page=contracts');
+    }
     /**
      * Store a newly created resource in storage.
      *
@@ -45,9 +84,50 @@ class PlayerController extends Controller
      */
     public function show($id)
     {
-        //
+        $player=Player::where('id','=',$id)->first();
+        $team = Team::where('id','=',$player->team)->first();
+        if($player->position!='G')
+        {
+            $stats = Field::where('player','=',$id)->cursor('id','desc');
+            if(count($stats)>0)
+            {
+                $last = Field::where('player','=',$id)->orderBy('id','desc')->first();
+                return view('playerView',array('set' => 1, 'point' => $last->season,'player' => $player, 'team' => $team, 'stats' => $stats));
+            }
+            else
+            {
+                return view('playerView',array('set' => 0, 'player' => $player, 'team' => $team));
+            }
+        }
+        else
+        {
+            $stats = Goalie::where('player','=',$id)->cursor('id','desc');
+            if(count($stats)>0)
+            {
+                $last = Goalie::where('player','=',$id)->orderBy('id','desc')->first();
+                return view('playerView',array('set' => 1, 'point' => $last->season,'player' => $player, 'team' => $team, 'stats' => $stats));
+            }
+            else
+            {
+                return view('playerView',array('set' => 0, 'player' => $player, 'team' => $team));
+            }
+        }
     }
-
+    public function byGame($id,$season)
+    {
+        $player=Player::where('id','=',$id)->first();
+        $team = Team::where('id','=',$player->team)->first();
+        if($player->position!='G')
+        {
+            $stats = Field::where('player','=',$id)->where('season','=',$season)->cursor('id','desc');
+            return view('playerView',array('set' => 2,'player' => $player, 'team' => $team, 'stats' => $stats));
+        }
+        else
+        {
+            $stats = Goalie::where('player','=',$id)->cursor('id','desc');
+            return view('playerView',array('set' => 2,'player' => $player, 'team' => $team, 'stats' => $stats));
+        }
+    }
     /**
      * Show the form for editing the specified resource.
      *
