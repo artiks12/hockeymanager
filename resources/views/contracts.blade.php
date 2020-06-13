aaedb6c91c1fb8c2f14807d2b28bb8f4955d644b
artiks12 <58003415+artiks12@users.noreply.github.com>
2020-06-13 12:06:44 +0300

    Created contracts.blade.php

---
+++ resources/views/contracts.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.copy')
+<style>
+    div.games
+    {
+        display:none;
+    }
+</style>
+
+@section('content')
+<div class="row justify-content-center" style='margin-top:30px;'>
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header">Sign a player</div>
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
