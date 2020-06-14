c4023822101b2aaf1ae4924a5d6db2ce7fab22b1
artiks12 <58003415+artiks12@users.noreply.github.com>
2020-06-14 21:49:47 +0300

    Update options.blade.php
    End of Day

--- resources/views/options.blade.php
+++ resources/views/options.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.copy')
-
+<style>
+    div.games
+    {
+        display:none;
+    }
+</style>
 @section('content')
 <div class="row justify-content-center" style='margin-top:30px;'>
     <div class="col-md-8">
@@ -10,7 +15,8 @@
             <div class="card-body">
                 <div class="makeSeason"><a href="{{ action('LeagueController@new',$id) }}">Make a New Season</a></div>
                 <div class="addTeam"><a href="{{ action('LeagueController@add',$id) }}">Add Teams To League</a></div>
-                <div class="addTeam"><a href="{{ action('GameController@create',$id) }}">Add Games To season</a></div>
+                <div class="addGame"><a href="{{ action('GameController@create',$id) }}">Add Games To season</a></div>
+                <div class="addPlayer"><a href="{{ action('PlayerController@create',$id) }}">Add Player To League</a></div>
             </div>
         </div>
     </div>
