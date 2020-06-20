5246ff9b939698325399d0d31892d4e658d6ad07
artiks12 <58003415+artiks12@users.noreply.github.com>
2020-06-20 19:28:59 +0300

    Add files via upload

---
+++ resources/lang/lv/messages.php
@@ -0,0 +1,222 @@
+<?php
+
+return [
+
+    'leagues' => 'Līgas',
+    'teams' => 'Komandas',
+    'players' => 'Spēlētāji',
+    'games' => 'Spēles',
+    'my_league' => 'Mana Līga',
+    'my_team' => 'Mana Komanda',
+    'next_game' => 'Nākamā Spēle',
+    'admin' => 'Administrators',
+    'languages' => 'Valodas',
+    'english' => 'Angļu',
+    'latvian' => 'Latviešu',
+    'login' => 'Pierakstīties',
+    'register' => 'Reģistrēties',
+    'logout' => 'Izrakstīties',
+    'today_games' => "Šodienas Spēles",
+    'no_games' => 'Šodien nav spēļu.',
+
+    //login.blade.php
+    'e-mail_address' => 'E-Pasta Adrese',
+    'password' => 'Parole',
+    'remember_me' => 'Atcerēties Mani',
+    'forgot_your_password' => 'Aizmirsi savu paroli?',
+    
+    //register.blade.php
+    'name' => 'Vārds',
+    'confirm_password' => 'Apliecini paroli',
+    'role' => 'Loma',
+    'select_role' => 'Izvēlies lomu:',
+    'commisioner' => 'Līgas Priekšsēdētājs',
+    'manager' => 'Komandas Menedžeris',
+    'statistician' => 'Statistiķis',
+    
+    //verify.blade.php
+    'verify_your_email_address' => 'Apstipriniet savu e-pastu.',
+    'a_fresh_verification_link_has_been_sent_to_your_email_address' => 'Jauns apstiprinājuma links tika nosūtīts uz jūsu e-pasta adresi.',
+    'before_proceding_please_check_your_email_for_a_verification_link' => 'Pirms turpinat, lūdzu pārbaudiet, vai jūsu e-pastā ir apstiprinājuma links.',
+    'if_you_did_not_receive_the_email' => 'Ja jūs nesaņēmāt e-pastu',
+    'click_here_to_request_another' => 'klikšķiniet šeit lai pieprasītu vēlvienu',
+    
+    //confirm.blade.php
+    'please_confirm_your_password_before_continuing' => 'Lūdzu aplieciniet savu paroli pirms turpinat.',
+    
+    //email.blade.php
+    'reset_password' => 'Atiestatīt paroli',
+    'send_password_reset_link' => 'Nosūtīt paroles atiestatīšanas linku.',
+    
+    //addTeam.blade.php
+    'add_team' => 'Pievienot Komandu',
+    'add_teams' => 'Pievienot Komandas',
+    'add' => 'Pievienot',
+    
+    //admin.blade.php
+    'admin_menu' => 'Admina izvelne',
+    'create_player' => 'Izveidot Spēlētāju',
+    'remove_user' => 'Noņemt Lietotāju',
+    'give_league' => 'Iedot Līgu',
+    'give_team' => 'Iedot Komandu',
+    'create_league' => 'Izveidot Līgu',
+    'create_team' => 'Izveidot Spēlētāju',
+    'surname' => 'Uzvārds',
+    'birthday' => 'Dzimšanas Diena',
+    'height' => 'Augums',
+    'weight' => 'Svars',
+    'position' => 'Pozīcīja',
+    'goalie' => 'Vārtsargs',
+    'defenceman' => 'Aizsargs',
+    'forward' => 'Uzbrucējs',
+    'goalies' => 'Vārtsargi',
+    'defencemen' => 'Aizsargi',
+    'forwards' => 'Uzbrucēji',
+    'remove' => 'Noņemt',
+    'user' => 'Lietotājs',
+    'league' => 'Līga',
+    'team' => 'Komanda',
+    'give' => 'Dot',
+    'there_is_nothing_you_can_do' => 'Te nav nekas, ko tu vari darīt.',
+    'create' => 'Izveidot',
+    
+    //contracts.blade.php
+    'sign_a_player' => 'Parakstīt spēlētāju',
+    'this_player_already_has_a_team' => 'Šim spēlētājam jau ir komanda',
+    'sign' => 'Parakstīt',
+    'number' => 'Numurs',
+    'years' => 'Gadi',
+    'total_salary' => 'Kopējā alga',
+    
+    //gameView.blade.php
+    'vs' => 'pret',
+    '1st' => '1.',
+    '2nd' => '2.',
+    '3rd' => '3.',
+    'ot' => 'OT',
+    'shootout' => 'Pēcspēles metieni',
+    'log' => 'Ieraksti',
+    'stats' => 'Statistikas',
+    'total' => 'Kopā',
+    'update' => 'Atjaunot',
+    'time' => 'Laiks',
+    'event' => 'Notikums',
+    'description' => 'Apraksts',
+    'goal' => 'Vārti',
+    'penalty' => 'Noraidījums',
+    'minutes_for' => 'minūtes par',
+    'scorer' => 'Iemeta',
+    'assisted' => 'Piespēlēja',
+    'empty_net' => 'Tukšos vārtos',
+    'gets' => 'dabū',
+    'sog' => 'Metieni pa Vārtiem',
+    'goals_allowed' => 'Ielaistie Vārti',
+    'player' => 'Spēlētājs',
+    'goals' => 'Vārti',
+    'assists' => 'Piespēles',
+    'points' => 'Punkti',
+    'pim' => 'Soda Minūtes',
+    'time_on_ice' => 'Nospēlētais laiks',
+    'roster_upload' => 'Ielādēt sastāvu',
+    'add_penalty' => 'Pieienot noraidījumu',
+    'add_goal' => 'Pievienot vārtus',
+    'shutouts' => 'Sausās spēles',
+    'wins' => 'Uzvaras',
+    'loses' => 'Zaudes',
+    'seconds' => 'Sekundes',
+    'faceoffs' => 'Iemetieni',
+    'faceoffs_won' => 'Uzvarētie iemetieni',
+    'shots' => 'Metieni',
+    'blocked_shots' => 'Bloķētie metieni',
+    'second' => 'Sekunde',
+    '1st_assist' => '1. piespēle',
+    '2nd_assist' => '2. piespēle',
+    'reason' => 'Iemesls',
+    'minutes' => 'Minūtes',
+    'finnish' => 'Beigt',
+    'insert_protocols' => 'Ievadi protokolus',
+    'all_protocols_have_been_uploaded' => 'Visi protokoli ir augšupieladēti',
+    
+    //games.blade.php
+    'game_type' => 'Spēles tips',
+    'date' => 'Datums',
+    'search' => 'Meklēt',
+    'regular_season' => 'Regulārā sezona', 
+    'play-off' => 'Izslēgšanas spēles',
+    'all' => 'visi',
+    'list' => 'Saraksts',
+    
+    //leagueView.blade.php
+    'there_are_no_seasons_for_this_league' => 'Šai līgai nav nevienas sezonas.',
+    
+    //myLeague.blade.php
+    'league_maker' => 'Līgas veidotājs',
+    
+    
+    //myTeam.blade.php
+    'menu' => 'Izvelne',
+    'for' => 'priekš',
+    'for_season' => 'sezonas',
+    'season' => 'sezona',
+    'team_roster' => 'Komandas sastāvs',
+    'free_agency' => 'Brīvie aģenti',
+    'trade_center' => 'Maiņu centrs',
+    'contracts' => 'Līgumi',
+    'player_statistics' => 'Spēlētāju statistikas',
+    'home' => 'Mājas',
+    'next_game' => 'Nākamā spēle',
+    'hosting' => 'Uzņem',
+    'at' => 'pie',
+    'on' => '',
+    'upload_roster' => 'Pievienot sastāvu',
+    'add_statistician' => 'Pievienot statistiķi',
+    'roster' => 'Sastāvs',
+    'cap_hit' => 'Līguma summa',
+    'there_are_no_free_agents' => 'Nav brīvo aģentu.',
+    'there_are_no_games' => 'Nav spēļu.',
+    'goals_aggainst_average' => 'Vidēji ielaistie vārti',
+    'save_percentage' => 'Procentuāli atvairītie metieni',
+    'this_team_does_not_belong_to_you' => 'Jums nepieder šī komanda!',
+    'warning' => 'Brīdinājums',
+    'you_cannot_access_this_page' => 'Jūs nevarat piekļūt šai lapai!',
+    'extend' => 'Pagarināt',
+    'release' => 'Atlaist',
+    
+    //newSeson.blade.php
+    'there_are_no_teams_to_add' => 'Nav komandu, kuras var pievienot.',
+    'new_season' => 'Jauna sezona',
+    
+    //options.blade.php
+    'make_new_season' => 'Izveidot jaunu sezonu',
+    'add_teams_to_league' => 'Pievieot komandas līgai',
+    'add_games_to_season' => 'Izveidot spēles sezonai',
+    'add_player_to_league' => 'Izveidot spēlētāju līgai',
+
+    //playerCreate.blade.php
+    'create_player' => 'Izveidot spēlētāju',
+    'there_are_no_teams_in_the_league_to_add_players_to' => 'Līgā nav nevienas komandas, kurai var pievienot spēlētāju!',
+    
+    //players.blade.php
+    'filter' => 'Filtrs',
+    'there_are_no_players_that_match_our_records' => 'Nav neviena spēlētāja, kas sakrīt ar mūsu ierakstiem!',
+    'free_agent' => 'Brīvais aģents',
+    
+    //seasonView.blade.php
+    'options' => 'Iespējas',
+    'team_statistics' => 'Komandu Statistikas',
+    'overtimes' => 'Papildlaiki',
+    'goal_difference' => 'Vārtu starpība',
+    
+    //teamView.blade.php
+    'latest_season' => 'Pēdējā sezona',
+    'none' => 'neviens',
+    
+    //teams.blade.php
+    'teams_with_no_league' => 'Komandas bez līgām',
+    
+    //playerView.blade.php
+    'there_are_no_statistics_from_previous_seasons' => 'Nav statistiku no iepriekšējām sezonām.',
+    'this_player_has_no_statistics' => 'Šim spēlētājam nav statistiku.',
+    'previous_seasons' => 'Iepriekšējās sezonas',
+    'game' => 'spēle',
+];
