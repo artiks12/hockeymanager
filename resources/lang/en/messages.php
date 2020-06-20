3bac8db250367502df08a8c34afc87e01005d53d
artiks12 <58003415+artiks12@users.noreply.github.com>
2020-06-20 19:29:41 +0300

    Add files via upload

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,226 @@
+<?php
+
+return [
+
+    //copy.blade.php
+    'leagues' => 'Leagues',
+    'teams' => 'Teams',
+    'players' => 'Players',
+    'games' => 'Games',
+    'my_league' => 'My League',
+    'my_team' => 'My team',
+    'next_game' => 'Next Game',
+    'admin' => 'Administrator',
+    'languages' => 'Languages',
+    'english' => 'English',
+    'latvian' => 'Latvian',
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'Logout',
+    'today_games' => "Today's Games",
+    'no_games' => 'There are no games today.',
+    
+    //login.blade.php
+    'e-mail_address' => 'E-Mail Address',
+    'password' => 'Password',
+    'remember_me' => 'Remember Me',
+    'forgot_your_password' => 'Forgot Your Password?',
+    
+    //register.blade.php
+    'name' => 'Name',
+    'confirm_password' => 'Confirm Password',
+    'role' => 'Role',
+    'select_role' => 'Select Role:',
+    'commisioner' => 'League Commisioner',
+    'manager' => 'Team Manager',
+    'statistician' => 'Statistician',
+    
+    //verify.blade.php
+    'verify_your_email_address' => 'Verify Your Email Address',
+    'a_fresh_verification_link_has_been_sent_to_your_email_address' => 'A fresh verification link has been sent to your email address.',
+    'before_proceding_please_check_your_email_for_a_verification_link' => 'Before proceeding, please check your email for a verification link.',
+    'if_you_did_not_receive_the_email' => 'If you did not receive the email',
+    'click_here_to_request_another' => 'click here to request another',
+    
+    //confirm.blade.php
+    'please_confirm_your_password_before_continuing' => 'Please confirm your password before continuing.',
+    
+    //email.blade.php
+    'reset_password' => 'Reset Password',
+    'send_password_reset_link' => 'Send Password Reset Link',
+    
+    //addTeam.blade.php
+    'add_team' => 'Add Team',
+    'add_teams' => 'Add Teams',
+    'add' => 'Add',
+    
+    //admin.blade.php
+    'admin_menu' => 'Admin Menu',
+    'create_player' => 'Create a Player',
+    'remove_user' => 'Remove a User',
+    'give_league' => 'Give a League',
+    'give_team' => 'Give a Team',
+    'create_league' => 'Create a League',
+    'create_team' => 'Create a Team',
+    'surname' => 'Surname',
+    'birthday' => 'Birthday',
+    'height' => 'Height',
+    'weight' => 'Weight',
+    'position' => 'Position',
+    'goalie' => 'Goalie',
+    'defenceman' => 'Defenceman',
+    'forward' => 'Forward',
+    'goalies' => 'Goalies',
+    'defencemen' => 'Defencemen',
+    'forwards' => 'Forwards',
+    'remove' => 'Remove',
+    'user' => 'User',
+    'league' => 'League',
+    'team' => 'Team',
+    'give' => 'Give',
+    'there_is_nothing_you_can_do' => 'There is nothing you can do.',
+    'create' => 'Create',
+    
+    //contracts.blade.php
+    'sign_a_player' => 'Sign a player',
+    'this_player_already_has_a_team' => 'This player already has a team',
+    'sign' => 'Sign',
+    'number' => 'Number',
+    'years' => 'Years',
+    'total_salary' => 'Total Salary',
+    
+    //gameView.blade.php
+    'vs' => 'vs',
+    '1st' => '1st',
+    '2nd' => '2nd',
+    '3rd' => '3rd',
+    'ot' => 'OT',
+    'shootout' => 'Shootout',
+    'log' => 'Log',
+    'stats' => 'Stats',
+    'total' => 'Total',
+    'update' => 'Update',
+    'time' => 'Time',
+    'event' => 'Event',
+    'description' => 'Description',
+    'goal' => 'Goal',
+    'penalty' => 'Penalty',
+    'minutes_for' => 'minutes for',
+    'scorer' => 'Scorrer',
+    'assisted' => 'Assisted',
+    'goalie' => 'Goalie',
+    'empty_net' => 'Empty net',
+    'gets' => 'gets',
+    'sog' => 'Shots on Goal',
+    'goals_allowed' => 'Goals Allowed',
+    'player' => 'Player',
+    'goals' => 'Goals',
+    'assists' => 'Assists',
+    'points' => 'Points',
+    'pim' => 'Penalties in minutes',
+    'time_on_ice' => 'Time on Ice',
+    'roster_upload' => 'Roster upload',
+    'add_penalty' => 'Add Penalty',
+    'add_goal' => 'Add Goal',
+    'shutouts' => 'Shutouts',
+    'wins' => 'Wins',
+    'loses' => 'Loses',
+    'seconds' => 'Seconds',
+    'faceoffs' => 'Faceoffs',
+    'faceoffs_won' => 'Faceoffs Won',
+    'shots' => 'Shots',
+    'blocked_shots' => 'Blocked Shots',
+    'second' => 'Second',
+    '1st_assist' => '1st Assist',
+    '2nd_assist' => '2nd Assist',
+    'reason' => 'Reason',
+    'minutes' => 'Minutes',
+    'finnish' => 'Finnish',
+    'insert_protocols' => 'Insert Protocols',
+    'all_protocols_have_been_uploaded' => 'All protocols have been uploaded',
+    
+    //games.blade.php
+    'game_type' => 'Game Type',
+    'date' => 'Date',
+    'search' => 'Search',
+    'regular_season' => 'Regular Season', 
+    'play-off' => 'Play-off',
+    'all' => 'all',
+    'list' => 'List',
+    
+    //leagueView.blade.php
+    'there_are_no_seasons_for_this_league' => 'There are no seasons for this league.',
+    
+    //myLeague.blade.php
+    'league_maker' => 'League Maker',
+    
+    
+    //myTeam.blade.php
+    'menu' => 'Menu',
+    'for' => 'for',
+    'season' => 'season',
+    'team_roster' => 'Team Roster',
+    'free_agency' => 'Free Agency',
+    'trade_center' => 'Trade Center',
+    'contracts' => 'Contracts',
+    'player_statistics' => 'Player Statistics',
+    'home' => 'Home',
+    'next_game' => 'Next Game',
+    'hosting' => 'Hosting',
+    'at' => 'at',
+    'on' => 'on',
+    'upload_roster' => 'Upload Roster',
+    'add_statistician' => 'Add a Statistician',
+    'roster' => 'Roster',
+    'cap_hit' => 'Cap Hit',
+    'there_are_no_free_agents' => 'There are no free agents.',
+    'there_are_no_games' => 'There are no games.',
+    'goals_aggainst_average' => 'Goals Aggainst Average',
+    'save_percentage' => 'Save Percentage',
+    'this_team_does_not_belong_to_you' => 'This team does not belong to you!',
+    'warning' => 'Warning',
+    'you_cannot_access_this_page' => 'You cannot access this page!',
+    'extend' => 'Extend',
+    'release' => 'Release',
+    
+    //newSeson.blade.php
+    'there_are_no_teams_to_add' => 'There are no teams to add.',
+    'new_season' => 'New Season',
+    
+    //options.blade.php
+    'make_new_season' => 'Make a New Season',
+    'add_teams_to_league' => 'Add Teams To League',
+    'add_games_to_season' => 'Create Games to Season',
+    'add_player_to_league' => 'Create Player to League',
+
+    //playerCreate.blade.php
+    'create_player' => 'Create a Player',
+    'there_are_no_teams_in_the_league_to_add_players_to' => 'There are no teams in the league to add players to!',
+    
+    //players.blade.php
+    'filter' => 'Filter',
+    'there_are_no_players_that_match_our_records' => 'There are no players that match our records!',
+    'free_agent' => 'Free Agent',
+    
+    //seasonView.blade.php
+    'options' => 'Options',
+    'team_statistics' => 'Team Statistics',
+    'overtimes' => 'Overtimes',
+    'goal_difference' => 'Goal Differnece',
+    
+    //teamView.blade.php
+    'latest_season' => 'Latest Season',
+    'none' => 'None',
+    
+    //teams.blade.php
+    'teams_with_no_league' => 'Teams With no League',
+    
+    //playerView.blade.php
+    'there_are_no_statistics_from_previous_seasons' => 'There are no statistics from previous seasons.',
+    'this_player_has_no_statistics' => 'This player has no statistics.',
+    'previous_seasons' => 'Previous Seasons',
+    'game' => 'game',
+    
+    
+    
+];
